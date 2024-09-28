3c38fb54030ec6d4311f1e46b2689ef5d905b99a
David Gu <dgu42@gatech.edu>
2024-09-27 22:27:24 -0400

    Stuff

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from flask import request, jsonify
+
 app = Flask(__name__)
 
 
@@ -7,6 +9,18 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    print("Username: ", username, "Password: ", password)
+    return "Login"
+
+@app.route("/register")
+def register():
+    return "Register"
 
 if __name__ == "__main__":
     app.run()
