2b6838da8089759b34718f3c4fe3333cf4fb528f
Ritwic <ritwic.verma.sachan@gmail.com>
2024-09-28 00:13:43 -0400

    added text extract

---
+++ backend/lib/textExtract.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from pypdf import PdfReader
+
+load_dotenv()
+
+OCR_API_KEY = os.getenv('OCR_API_KEY')
+rel_path = "../input.pdf"
+script_dir = os.path.dirname(__file__)
+PDF_PATH = os.path.join(script_dir, rel_path)
+def getText(path):
+    reader = PdfReader(path)
+    text = reader.pages[0].extract_text()
+    return text
+    
+print(getText(PDF_PATH))
