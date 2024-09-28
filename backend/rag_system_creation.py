bdfbb47eb075ab17954f88db5ef7fefd9b149d3e
amishasao <mintysao@gmail.com>
2024-09-28 00:10:32 -0400

    started creating rag

---
+++ backend/rag_system_creation.py
@@ -0,0 +1,27 @@
+from datasets import load_dataset
+
+import pandas as pd
+
+# https://huggingface.co/datasets/AIatMongoDB/embedded\_movies
+
+dataset = load_dataset("wasiqnauman/medical-diagnosis-synthetic")
+
+# Convert the dataset to a pandas dataframe
+
+dataset_df = pd.DataFrame(dataset['train'])
+
+dataset_df.head(5)
+
+# Remove data point where plot column is missing
+
+dataset_df = dataset_df.dropna(subset=['plot'])
+
+print("\nNumber of missing values in each column after removal:")
+
+print(dataset_df.isnull().sum())
+
+# Remove the plot_embedding from each data point in the dataset as we are going to create new embeddings with the new OpenAI embedding Model "text-embedding-3-small"
+
+dataset_df = dataset_df.drop(columns=['plot_embedding'])
+
+dataset_df.head(5)
