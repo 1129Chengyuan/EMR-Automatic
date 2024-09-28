0ae20fc8a07297daf3eef6dcdf0c3017ee3a1173
David Gu <dgu42@gatech.edu>
2024-09-28 19:27:50 -0400

    stuff

--- backend/lib/textExtract.py
+++ backend/lib/textExtract.py
@@ -11,4 +11,18 @@ def getText():
     reader = PdfReader(PDF_PATH)
     text = reader.pages[0].extract_text()
     return text
-    
+
+def getText(filePath):
+    rel_path = filePath
+    script_dir = os.path.dirname(__file__)
+    PDF_PATH = os.path.join(script_dir, rel_path)
+    reader = PdfReader(PDF_PATH)
+    text = reader.pages[0].extract_text()
+    return text
+
+def displayToPage(filePath):
+    text = getText(filePath)
+
+    # RAG Stuff
+
+    return "Placeholder body text"
