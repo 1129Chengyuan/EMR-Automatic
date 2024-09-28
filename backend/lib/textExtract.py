d167799d8de470884f43f41a92e85247cc88e7d7
Ritwic <ritwic.verma.sachan@gmail.com>
2024-09-28 16:03:31 -0400

    updated textExtract

--- backend/lib/textExtract.py
+++ backend/lib/textExtract.py
@@ -4,12 +4,11 @@ from pypdf import PdfReader
 
 load_dotenv()
 
-rel_path = "../input.pdf"
-script_dir = os.path.dirname(__file__)
-PDF_PATH = os.path.join(script_dir, rel_path)
-def getText(path):
-    reader = PdfReader(path)
+def getText():
+    rel_path = "../input.pdf"
+    script_dir = os.path.dirname(__file__)
+    PDF_PATH = os.path.join(script_dir, rel_path)
+    reader = PdfReader(PDF_PATH)
     text = reader.pages[0].extract_text()
     return text
     
-print(getText(PDF_PATH))
